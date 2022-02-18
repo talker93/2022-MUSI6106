6d0bd9b068ee875bdcc60bbbc7b3433734b40a28
Shan Jiang <jiangshan789123@hotmail.com>
2022-02-18 10:08:39 -0500

    basic change on ringbuffer

--- src/inc/RingBuffer.h
+++ src/inc/RingBuffer.h
@@ -13,15 +13,21 @@ class CRingBuffer
 {
 public:
     explicit CRingBuffer(int iBufferLengthInSamples) :
-        m_iBuffLength(iBufferLengthInSamples)
+        m_iBuffLength(iBufferLengthInSamples),
+        m_iReadIdx(0),
+        m_iWriteIdx(0),
+        m_ptBuff(0)
     {
         assert(iBufferLengthInSamples > 0);
-
+        m_ptBuff = new T [m_iBuffLength];
+        reset();
         // allocate and init
     }
 
     virtual ~CRingBuffer()
     {
+        delete [] m_ptBuff;
+        m_ptBuff = 0;
         // free memory
     }
 
@@ -31,6 +37,8 @@ public:
     */
     void putPostInc (T tNewValue)
     {
+        put(tNewValue);
+        incIdx(m_iWriteIdx);
     }
 
     /*! add a new value of type T to write index
@@ -39,6 +47,7 @@ public:
     */
     void put(T tNewValue)
     {
+        m_ptBuff[m_iWriteIdx]   = tNewValue;
     }
     
     /*! return the value at the current read index and increment the read pointer
@@ -46,7 +55,10 @@ public:
     */
     T getPostInc()
     {
-        return static_cast<T>(-1);
+        T tValue = get();
+        incIdx(m_iReadIdx);
+        return tValue;
+//        return static_cast<T>(-1);
     }
 
     /*! return the value at the current read index
@@ -54,7 +66,21 @@ public:
     */
     T get() const
     {
-        return static_cast<T>(-1);
+        if (fOffset == 0.f)
+            return m_ptBuff[m_iReadIdx];
+        else
+        {
+            assert (std::abs(fOffset) <= m_iBuffLength);
+
+            int iRead   = m_iReadIdx + static_cast<int>(std::floor(fOffset));
+            while (iRead > m_iBuffLength-1)
+                iRead  -= m_iBuffLength;
+            while (iRead < 0)
+                iRead  += m_iBuffLength;
+
+            return m_ptBuff[iRead];
+        }
+//        return static_cast<T>(-1);
     }
     
     /*! set buffer content and indices to 0
@@ -114,7 +140,20 @@ public:
 private:
     CRingBuffer();
     CRingBuffer(const CRingBuffer& that);
+    
+    void incIdx (int &iIdx, int iOffset = 1)
+    {
+        while((iIdx + iOffset) < 0)
+        {
+            iOffset += m_iBuffLength;
+        }
+        iIdx = (iIdx + iOffset) % m_iBuffLength;
+    }
 
-    int m_iBuffLength;              //!< length of the internal buffer
+    int m_iBuffLength,              //!< length of the internal buffer
+        m_iReadIdx,
+        m_iWriteIdx;
+    
+    T   *m_ptBuff;
 };
 #endif // __RingBuffer_hdr__
