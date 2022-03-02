298bf4d9ffda914433dbe1a9e3a85a362b94dcbf
jocekav <jkavanagh3@gatech.edu>
2022-03-01 23:43:15 -0500

    Add vibrato test framework and test cases

--- src/Tests/Tests/Test_Vibrato.cpp
+++ src/Tests/Tests/Test_Vibrato.cpp
@@ -13,6 +13,17 @@
 
 #include "Vibrato.h"
 
+namespace vibrato_test {
+    void CHECK_ARRAY_CLOSE(float* buffer1, float* buffer2, int iLength, float fTolerance)
+    {
+        for (int i = 0; i < iLength; i++)
+        {
+            EXPECT_NEAR(buffer1[i], buffer2[i], fTolerance);
+        }
+    }
+
+}
+
 SUITE(Vibrato)
 {
     struct VibratoData
@@ -21,17 +32,108 @@ SUITE(Vibrato)
         {
             // setup
             // e.g., allocate a vibrato object and test signal (newly created for each test case)
+            
+            pVibrato(0),
+            ppfAudioData(0),
+            ppfOutputData(0),
+            
+            ppfTempIn(0),
+            ppfTempOut(0),
+            
+            fMaxWidthSecs(0.1),
+            fSampleRate(44100),
+            iNumChannels(2),
+            iBlockSize(1024),
+            fModFreq(10),
+            fModWidth(0.05)
+            
+            {
+                CVibrato::create(pVibrato);
+                pVibrato -> CVibrato::init(fMaxWidthInS, fSampleRateInHz, iNumChannels, fModFreq, fModWidth);
+                
+                ppfAudioData  = new float*[iNumChannels];
+                ppfOutputData = new float*[iNumChannels];
+                
+                ppfTempIn  = new float*[iNumChannels];
+                ppfTempOut = new float*[iNumChannels];
+                
+                float fTestSigFreq = 440.F;
+                
+                for (int i = 0; i < iNumChannels; i++)
+                {
+                    ppfAudioData[i] = new float[iBlockSize];
+                    ppfOutputData[i] = new float[iBlockSize];
+                    
+                    ppfTempIn[i]  = new float[iBlockSize];
+                    ppfTempOut[i] = new float[iBlockSize];
+
+                    CSynthesis::generateSine(ppfAudioData[i], fTestSigFreq, iSampleRate, iBlockSize);
+                
+            }
+            
         }
 
         ~VibratoData()
         {
             // teardown
             // e.g., deallocate the vibrato object and test signal
+            
+            for (int i = 0; i < iNumChannels; i++)
+            {
+                delete[] ppfAudioData[i];
+                delete[] ppfOutputData[i];
+                
+                delete[] ppfTempIn[i];
+                delete[] ppfTempOut[i];
+            }
+            delete[] ppfAudioData;
+            delete[] ppfOutputData;
+            
+            delete[] ppfTempIn;
+            delete[] ppfTempOut;
+            
+            CVibrato::destroy(pVibrato);
+
         }
 
         // e.g., a reusable process() function
+        void testProcess(float** ppfOutputData, int iBlockSize, int iStartSample = 0,)
+            {
+                for (int i = 0; i < iNumChannels; i++)
+                {
+                    for (int j = 0; j < iBlockSize; j++)
+                    {
+                        ppfTempIn[i][j] = ppfAudioData[i][iStartSample + j];
+                    }
+                }
+
+                pVibrato->process(ppfTempIn, ppfTempOut, iBlockSize);
+
+                for (int i = 0; i < iNumChannels; i++)
+                {
+                    for (int j = 0; j < iBlockSize; j++)
+                    {
+                        ppfOutputData[i][iStartSample + j] = ppfTempOut[i][j];
+                    }
+                }
+            }
 
         // e.g., a member vibrato object to be reused in each test
+            
+        CVibrato::Vibrato   *pVibrato;
+        float**             ppfAudioData;
+        float**             ppfOutputData;
+            
+        float**             ppfTempIn;
+        float**             m_ppfTempOut;
+            
+        float               fMaxWidthSecs;
+        float               fSampleRate;
+        int                 iNumChannels;
+        int                 iBlockSize;
+        float               fModFreq;
+        float               fModWidth;
+            
     };
 
     TEST(MyTestWithNoFixture)
@@ -39,10 +141,103 @@ SUITE(Vibrato)
         // e.g., allocate & deallocate local data for testing
     }
 
-    TEST_FIXTURE(VibratoData, MyTestWithFixture)
+    // Output equals delayed input when modulation amplitude is 0.
+    TEST_FIXTURE(VibratoData, ModAmpZero)
     {
         // e.g., you can use the "VibratoData" contents
+        pVibrato -> reset();
+        
+        fMaxWidthSecs = 0.1;
+        fModWidth = 0;
+        
+        pVibrato -> init(fMaxWidthSecs, fSampleRate, iNumChannels, fModFreq, fModWidth);
+        
+        testProcess(ppfOutputData, iBlockSize);
+        
+        int iDelayInSamples = (int)(ceil(fModWidth * sampleRate))
+        
+        for (int i = 0; i < iNumChannels; i++)
+        {
+           for (int j = iDelayinSamples; j < iBlockSize; j++)
+           {
+               CHECK_ARRAY_CLOSE(ppfAudioData[i][j - iDelayInSamples], outputData[i][j], 1, 1e-3F);
+           }
+        }
     }
+       
+    // DC input stays DC ouput regardless of parametrization.
+    TEST_FIXTUREE(VibratoData, DCEqual)
+    {
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            CSynthesis::generateDc(m_ppfAudioData[i], iBlockSize);
+        }
+        
+        pVibrato -> reset();
+        
+        fMaxWidthSecs = 0.1;
+        fModWidth = 0.05;
+        
+        pVibrato -> init(fMaxWidthSecs, fSampleRate, iNumChannels, fModFreq, fModWidth);
+        
+        testProcess(ppfOutputData, iBlockSize);
+        
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            CHECK_ARRAY_CLOSE(ppfAudioData[i], outputData[i], iBlockSize, 1e-3F);
+        }
+    }
+    
+    // Varying input block size.
+    TEST_FIXTURE(VibratoData, VaryBlockSize)
+    {
+        float** ppfOutBlock  = new float*[iNumChannels];
+        
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            ppfOutBlock[i] = new float[iBlockSize];
+        }
+        
+        pVibrato -> reset();
+        
+        pVibrato -> init(fMaxWidthSecs, fSampleRate, iNumChannels, fModFreq, fModWidth);
+        
+        testProcess(ppfOutputData, iBlockSize);
+        testProcess(ppfOutBlock, 459);
+        
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            CHECK_ARRAY_CLOSE(ppfOutBlock[i], outputData[i], iBlockSize, 1e-3F)
+            delete[] ppfOutBlock[i];
+        }
+        
+        delete[] ppfOutBlock;
+    }
+    
+    // Zero input signal.
+    TEST_FIXTURE(VibratoData, ZeroInput)
+    {
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            CSynthesis::generateDc(m_ppfAudioData[i], iBlockSize, 0);
+        }
+        
+        pVibrato -> reset();
+        
+        fMaxWidthSecs = 0.1;
+        fModWidth = 0.05;
+        
+        pVibrato -> init(fMaxWidthSecs, fSampleRate, iNumChannels, fModFreq, fModWidth);
+        
+        testProcess(ppfOutputData, iBlockSize);
+        
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            CHECK_ARRAY_CLOSE(ppfAudioData[i], outputData[i], iBlockSize, 1e-3F);
+        }
+    }
+        
+    TEST_FIXTURE(VibratoData, )
 }
 
 #endif //WITH_TESTS
